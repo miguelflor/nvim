fdbb1bce853974493f834b5728db633c5a88cd01
Miguel Cid Flor <cidflor2004@gmail.com>
2026-05-10 13:54:46 +0100

    fix(lsp): Arduino-Nvim plugin is very bad a lot off bugs, I prefer to continue to build my own

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,20 +108,5 @@ return {
     config = function()
       require("config.lsp").setup_conform()
     end
-  },
-  {
-    "yuukiflow/Arduino-Nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      -- Load Arduino plugin for .ino files
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "arduino",
-        callback = function()
-          require("Arduino-Nvim")
-        end,
-      })
-    end,
   }
 }
