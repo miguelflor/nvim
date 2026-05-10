674399b71b4007519227363838bddc7688fd132f
Miguel Cid Flor <cidflor2004@gmail.com>
2026-05-10 13:53:23 +0100

    fix(ai): Keymap conflit, most of default opencode keymaps are good as it is

--- lua/config/ai.lua
+++ lua/config/ai.lua
@@ -33,14 +33,14 @@ M.opencode_setup = function()
 
   vim.o.autoread = true
 
-  vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
-    { desc = "Ask opencode…" })
-  vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
-    { desc = "Execute opencode action…" })
-  vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
-
-  vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
-    { desc = "Add range to opencode", expr = true })
+  -- vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
+  --   { desc = "Ask opencode…" })
+  -- vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
+  --   { desc = "Execute opencode action…" })
+  -- vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+  --
+  -- vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
+  --   { desc = "Add range to opencode", expr = true })
   vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
     { desc = "Add line to opencode", expr = true })
 
@@ -49,8 +49,8 @@ M.opencode_setup = function()
   vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end,
     { desc = "Scroll opencode down" })
 
-  vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
-  vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+  -- vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+  -- vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
 end
 
 return M
