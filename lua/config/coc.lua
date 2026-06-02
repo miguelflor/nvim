398ad91d99653208f92b00ea90b8e3810c29501e
Miguel Cid Flor <cidflor2004@gmail.com>
2026-06-02 02:16:16 +0100

    refactor(coc): Filetypes coc variable created

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -1,16 +1,17 @@
 local M = {}
 
 function M.setup()
+  local coc_filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" }
   -- Disable blink.cmp for coc filetypes
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+    pattern = coc_filetypes,
     callback = function()
       vim.b.completion = false
     end,
   })
 
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+    pattern = coc_filetypes,
     callback = function(ev)
       require("config.keymaps").coc(ev.buf)
     end,
