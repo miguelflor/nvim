52772e6bf0521bac42e394fcbd5d16d6f4b5ca74
Miguel Cid Flor <cidflor2004@gmail.com>
2026-06-18 15:33:43 +0100

    feat(ui): Change the tabs ui to more minimal

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -149,41 +149,24 @@ function M.tabby()
     line = function(line)
       return {
         {
-          { '  ', hl = theme.head },
-          line.sep('', theme.head, theme.fill),
+          line.sep(' ', theme.head, theme.fill),
         },
         line.tabs().foreach(function(tab)
           local hl = tab.is_current() and theme.current_tab or theme.tab
           return {
-            line.sep('', hl, theme.fill),
+            line.sep(' ', hl, theme.fill),
             tab.is_current() and '' or '󰆣',
             tab.number(),
             tab.name(),
             tab.close_btn(''),
-            line.sep('', hl, theme.fill),
+            line.sep(' ', hl, theme.fill),
             hl = hl,
             margin = ' ',
           }
         end),
-        line.spacer(),
-        line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-          return {
-            line.sep('', theme.win, theme.fill),
-            win.is_current() and '' or '',
-            win.buf_name(),
-            line.sep('', theme.win, theme.fill),
-            hl = theme.win,
-            margin = ' ',
-          }
-        end),
-        {
-          line.sep('', theme.tail, theme.fill),
-          { '  ', hl = theme.tail },
-        },
         hl = theme.fill,
       }
     end,
-    -- option = {}, -- setup modules' option,
   })
 end
 
